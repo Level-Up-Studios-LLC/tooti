9c97868b2aeff9e6256f06ebc81f4b933f86b214
Marc Joseph <marc@lvlupstudios.com>
2020-06-07 15:28:42 -0400

    First commit

---
+++ precache-manifest.a772b878403a91b856a4b987cfdcada5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9742adc9827e0fd7944842c98e24e73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4a65516e65063424043",
+    "url": "/static/css/2.cb7b0f97.chunk.css"
+  },
+  {
+    "revision": "b6f7811b310e84f15e56",
+    "url": "/static/css/main.a8b95442.chunk.css"
+  },
+  {
+    "revision": "f4a65516e65063424043",
+    "url": "/static/js/2.bac8cc1c.chunk.js"
+  },
+  {
+    "revision": "c560b744118e776c5a9fe14e0546e12a",
+    "url": "/static/js/2.bac8cc1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6f7811b310e84f15e56",
+    "url": "/static/js/main.b80f27cf.chunk.js"
+  },
+  {
+    "revision": "a44aaea8159dcc4f8f3e",
+    "url": "/static/js/runtime-main.695906cf.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
